63ac770ab1f56461bc38b65ae736015de667983c
adn87 <aaghadieh@gmail.com>
2023-10-28 05:27:15 -0400

    Snake_Game_002
    - creating the snake and setting up the starting position for it

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
-
 from turtle import Turtle, Screen
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("My Snake Game")
+starting_position = [(0,0), (-20,0), (-40,0)]
+
+for position in starting_position:
+    kaa = Turtle(shape="square")
+    kaa.color("white")
+    kaa.penup()
+    kaa.goto(position)
 
 
 
