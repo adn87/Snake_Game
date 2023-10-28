5266564cfcf354e05ab95e3b2b730740e7cf13d9
adn87 <aaghadieh@gmail.com>
2023-10-28 11:34:41 -0400

    Snake_Game_007
    - created a food class and inherited everything from the Turtle class.
    - used the random module to create random coordinates to create food
    -  created a new food object in the main class.
    - created a new function in the Food class to recreate the food object at a new coordinate when the snake and the food object collide
    - code clean up

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import time
 from turtle import Screen
 from snake import Snake
+from food import Food
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -9,6 +10,9 @@ screen.title("My Snake Game")
 screen.tracer(0)
 
 snake = Snake()
+food = Food()
+
+
 screen.listen()
 screen.onkey(key="Up", fun=snake.up)
 screen.onkey(key="Down", fun=snake.down)
@@ -21,5 +25,8 @@ while game_is_on:
     screen.update()
     time.sleep(0.1)
     snake.move()
+
+    if snake.head.distance(food) < 15:
+        food.refresh()
 
 screen.exitonclick()
