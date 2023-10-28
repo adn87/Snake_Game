d01b89000021d7c9b7d55969f460f335fe3b8256
adn87 <aaghadieh@gmail.com>
2023-10-28 06:43:55 -0400

    Snake_Game_006
    - used the listen() and onkey() method in the screen module to control the snake with key press

--- main.py
+++ main.py
@@ -9,6 +9,11 @@ screen.title("My Snake Game")
 screen.tracer(0)
 
 snake = Snake()
+screen.listen()
+screen.onkey(key="Up", fun=snake.up)
+screen.onkey(key="Down", fun=snake.down)
+screen.onkey(key="Left", fun=snake.left)
+screen.onkey(key="Right", fun=snake.right)
 
 game_is_on = True
 
